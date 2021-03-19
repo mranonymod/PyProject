c78736a76a69aa6545174460d72c6d9b8b91cde9
mranonymod <64246578+mranonymod@users.noreply.github.com>
2021-03-19 18:37:48 +0530

    modifications app.py

--- app.py
+++ app.py
@@ -1,33 +1,50 @@
 import sys
+import resources_rc
 from PyQt5 import QtWidgets as qtw
-from PyQt5 import QtCore as qtc 
+from PyQt5 import QtCore  
 from PyQt5 import QtGui as qtg 
 from PyQt5 import uic
 from PyQt5.uic import loadUi
 Ui_Start,baseClass=uic.loadUiType('Start.ui')
 Ui_Login,baseClass2=uic.loadUiType('Login.ui')
 
-class MainW(baseClass):
-    def __init__(self,*arg,**kwargs):
+class Start(baseClass):
+    sw=QtCore.pyqtSignal()
+    def __init__(self, *arg, **kwargs):
         super().__init__(*arg,**kwargs)
         #code start
         self.ui=Ui_Start()
         self.ui.setupUi(self)
-        self.ui.LoginB.clicked.connect(lambda : self.LnW())
         #code end
-        self.show()
-    def LnW(self,*arg,**kwargs):
+    def closeW(self):
+        self.sw.emit()
+class Login(baseClass2):
+    def __init__(self, *arg, **kwargs):
         super().__init__(*arg,**kwargs)
         #code start
-        loadUi("Login.ui",self)
         self.ui=Ui_Login()
         self.ui.setupUi(self)
-        self.show()
+        #code end
 
-if __name__ == '__main__':
+class Controller:
+
+    def __init__(self):
+        pass 
+    def show_start(self):
+        self.window=Start(windowTitle='Choose')
+        self.window.sw.connect(self.show_login)
+        self.window.show()
+    def show_login(self):
+        self.login = Login()
+        self.window.close()
+        self.login.show()
+        
+def main():
     app = qtw.QApplication(sys.argv)
-    w=MainW(windowTitle='hello')
+    w=Controller()
+    w.show_start()
     sys.exit(app.exec_())
 
-
+if __name__ == '__main__':
+    main()
 
