b438e3eccfbd2ca252552b2b0e547376b9abb059
mranonymod <64246578+mranonymod@users.noreply.github.com>
2021-03-24 21:35:14 +0530

    temp .sql file added

---
+++ PasswordManager.sql
@@ -0,0 +1,27 @@
+  
+CREATE DATABASE PasswordManager; 
+USE PasswordManager;
+
+CREATE TABLE Users( 
+	Username VARCHAR(64) PRIMARY KEY,
+	Name VARCHAR(128),
+	Email VARCHAR(128) NOT NULL UNIQUE,
+	Password VARCHAR(64)
+); 
+
+
+CREATE TABLE Passwords (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	Username VARCHAR(64) FOREIGN KEY (`Users`) REFERENCES `Users` (`Username`),
+	Service VARCHAR(128) NOT NULL, 
+    SharedID INT FOREIGN KEY (`SHDGRP`) REFERENCES `SHDGRP`(`SharedID`),
+	Passwords VARCHAR(256) NOT NULL,
+    SetOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+); 
+
+CREATE TABLE SHDGRP(
+    SharedID INT PRIMARY KEY,
+    Services VARCHAR(128) FOREIGN KEY (`Passwords`) REFERENCES `Passwords`(`Service`),
+    Username VARCHAR(64) FOREIGN KEY (`Users`) REFERENCES `Users` (`Username`)
+);
