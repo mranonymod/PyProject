8a4fbfa1b8d7c16ebcaae5e174e0c77ccb8f2ab4
mranonymod <64246578+mranonymod@users.noreply.github.com>
2021-05-10 14:49:30 +0530

    Personal Pwd Deletion

---
+++ DBdelpwd.py
@@ -0,0 +1,29 @@
+from DBconnect import *
+from icecream import ic
+
+class db1:
+    def __init__(self,username,service):
+        self.username = username
+        self.service=service
+        self.db=mydb
+    def checksh(self):
+        self.cur1=self.db.cursor()
+        self.cur1.execute('''SELECT SharedID FROM SHDGRP WHERE SharedID=(SELECT SharedID FROM Passwords WHERE Service = %s AND Username = %s)''',(self.service,self.username))
+        self.id=self.cur1.fetchone()
+        if(self.id):
+            return False
+        else:
+            return True
+
+    def delete(self):
+        if(self.checksh()):
+            self.cur2=self.db.cursor()
+            self.cur2.execute('''DELETE FROM Passwords WHERE Service = %s AND Username = %s;''',(self.service,self.username))
+            self.db.commit()
+            self.id2=self.cur2.rowcount
+            return True
+        else:
+            return False
+        
+'''new=db1("bruh","WIFI")
+print(new.delete())'''
